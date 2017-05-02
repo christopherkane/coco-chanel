5a98cf89632d7f3b022c8e78625c210f07181283
Christopher Kane <ckane386@gmail.com>
2017-05-02 21:51:35 +0100

    Setting default postgreSQL db, and the ability to override it from env

--- coco/settings.py
+++ coco/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,11 +76,12 @@ WSGI_APPLICATION = 'coco.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default="postgres:///coco-chanel", conn_max_age=500)
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 CHANNEL_LAYERS = {
     "default": {
